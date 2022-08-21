6c91a19be8187fb5e285ba6e4a615056143ebe99
ErRajeevShakya <rsbly730095@gmail.com>
2022-08-21 14:38:54 +0530

    initial commit

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../header/Header.jsx";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Layout;
