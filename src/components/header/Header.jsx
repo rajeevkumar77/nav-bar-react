5f880c16411829c6cd755607b7e7cdf60a2b5718
ErRajeevShakya <rsbly730095@gmail.com>
2022-08-21 14:47:31 +0530

    initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 
 import { Container } from "reactstrap";
-import logo from "../../assets/images/res-logo.png";
+import logo from "../../assets/image/res-logo.png";
 import { NavLink, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
